0ca2c050533c58dc4a4902459efc6d628d73991c
paula gamba <pausbal@gmail.com>
2025-05-01 21:59:30 +0200

    Añadida Documentación

--- src/practica/CalculosMatematicos.java
+++ src/practica/CalculosMatematicos.java
@@ -1,11 +1,21 @@
 package practica;
 
 import java.util.ArrayList;
-
+/**
+ * @author PaulaSanchez
+ * @since 22-04-2025
+ */
 public class CalculosMatematicos {
-	
+	/**
+	 * constructor
+	 */
 	public CalculosMatematicos() {		
 	}
+	/**
+	 * devuelve por pantalla el maximo común divisor de 2 números
+	 * @param numA int
+	 * @param numB int
+	 */
 	public void calcularMCD(int numA, int numB) {
 		int a = numA, b = numB;
 		while (b != 0) {
@@ -16,6 +26,11 @@ public class CalculosMatematicos {
 		System.out.println("El máximo común divisor de "+numA+ " y "+numB+" es " + a);
 		}
 
+	/**
+	 * 
+	 * @param numero int
+	 * calcula los factoriales de un numero y los devuelve por pantalla, además de dar un mensaje indicando si el número es o no primo, utilizando los metodos noesprimo y factoriales
+	 */
 
 	public void calculosRefactorizados(int numero) {
 		
@@ -42,8 +57,15 @@ public class CalculosMatematicos {
 			System.out.println("El número "+numero+ " NO es primo");
 		} else System.out.println("El número "+numero+ " SI es primo");
 	}
-
-	private boolean noEsPrimo(int numero, ArrayList<Integer> miArray, boolean noEsPrimo) {
+/**
+ *  metodo para comprobar si un numero es o no primo
+ * @param numero int
+ * @param miArray ArrayList
+ * @param noEsPrimo boolean 
+ * @return 
+ * devuelve un booleano, si el booleano es true el numero no es primo, y si es false el booleano es primo
+ */
+	public boolean noEsPrimo(int numero, ArrayList<Integer> miArray, boolean noEsPrimo) {
 		int j;
 		for (int  i = 2; i< numero ; i++) {
 			noEsPrimo = false;
@@ -56,8 +78,14 @@ public class CalculosMatematicos {
 		}
 		return noEsPrimo;
 	}
-
-	private boolean factorialesPrimos(boolean noEsPrimo, int j, int i) {
+/**
+ * metodo para comprobar que factoriales son primos
+ * @param noEsPrimo
+ * @param j int
+ * @param i int
+ * @return devuelve un booleano que determina si cada uno de los factoriales es primo
+ */
+	public boolean factorialesPrimos(boolean noEsPrimo, int j, int i) {
 		while(j<i) {
 			if(i%j==0) {
 				noEsPrimo = true;
